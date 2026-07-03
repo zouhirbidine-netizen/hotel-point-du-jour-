759c74283430430ba910eefa59a01fef2b25d5e8
Zouhir Bidine <zouhir@gmail.com>
2026-07-04 00:15:15 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 50) {
+        header.style.background = "rgba(0,0,0,0.95)";
+    } else {
+        header.style.background = "rgba(0,0,0,0.75)";
+    }
+});
